ede302431ac072da35bf04f5223e4fea88a19a23
Vladislav-Kanev <vokanev@edu.hse.ru>
2021-01-18 20:14:32 +0300

    fist try

--- include/MyStack.h
+++ include/MyStack.h
@@ -1,9 +1,60 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 by me
 #ifndef INCLUDE_MYSTACK_H_
 #define INCLUDE_MYSTACK_H_
 
-template<class T>
+template <typename T>
 class MyStack {
+ private:
+     unsigned int size;
+     int top;
+     T *value;
+
+ public:
+     explicit MyStack(unsigned int max_size) {
+         size = max_size;
+         top = 0;
+         value = new T[size];
+     }
+     MyStack(const MyStack& to_copy) {
+         size = to_copy.size;
+         top = to_copy.top;
+         value = new T[size];
+         for (unsigned i = 0; i < top; ++i) {
+             value[i] = to_copy.value[i];
+         }
+     }
+     ~MyStack() {
+         if (top != 0)
+            delete value;
+     }
+     T get() const {
+         if (top != 0)
+             return value[top - 1];
+     }
+     T pop() {
+        if (top == 0) throw - 1;
+        top -= 1;
+        return value[top];
+     }
+     void push(T element) {
+         if (isFull()) throw - 1;
+         value[top] = element;
+         top++;
+     }
+     bool isFull() const {
+         if (top == size)
+             return true;
+         else
+             return false;
+     }
+     bool isEmpty() const {
+         if (top == 0)
+             return true;
+         else
+             return false;
+     }
 };
+
+
 
 #endif  // INCLUDE_MYSTACK_H_
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -1,7 +1,94 @@
-// Copyright 2020 A.SHT
-#include "MyStack.h"
-#include "postfix.h"
+// Copyright 2021 by me
+#include "../include/MyStack.h"
+#include "../include/postfix.h"
+
+bool isOperator(char c) {
+    switch (c) {
+    case '+':
+        return true;
+    case '-':
+        return true;
+    case '*':
+        return true;
+    case '/':
+        return true;
+    case '(':
+        return true;
+    case ')':
+        return true;
+    default:
+        return false;
+    }
+}
+
+int priority(char c) {
+    switch (c) {
+    case '(':
+        return 0;
+    case ')':
+        return 1;
+    case '+':
+        return 2;
+    case '-':
+        return 2;
+    case '*':
+        return 3;
+    case '/':
+        return 3;
+    default:
+        return 4;
+    }
+}
 
 std::string infix2postfix(std::string infix) {
-  return infix;
+    std::string prefix("");
+    MyStack<std::string> stack(500);
+    for (size_t i = 0; i < infix.size(); ++i) {
+        if (infix[i] == ' ') {
+            continue;
+        }
+        if (isOperator(infix[i])) {
+            if (infix[i] == '(' || stack.isEmpty()) {
+                stack.push(std::string(1,infix[i]));
+            }
+            else {
+                if (priority(infix[i]) > priority(stack.get()[0]))
+                    stack.push(std::string(1, infix[i]));
+                else {
+                    while (!stack.isEmpty() &&
+                        priority(stack.get()[0]) >= priority(infix[i])) {
+                        prefix += stack.pop() + " ";
+                    }
+                    if (infix[i] == ')') {
+                        if (stack.get()[0] == '(') {
+                            stack.pop();
+                        }
+                    }
+                    else {
+                        stack.push(std::string(1, infix[i]));
+                    }
+                }
+            }
+        }
+        else {
+            std::string buff("");
+            while (infix[i] != ' ' && i < infix.size()) {
+                buff += infix[i];
+                i++;
+                if (i < infix.size() && infix[i] == ')') {
+                    i--;
+                    break;
+                }
+            }
+            prefix += buff + ' ';
+        }
+    }
+    while (!stack.isEmpty()) {
+        if (stack.get() == "(") {
+            stack.pop();
+            continue;
+        }
+        prefix += stack.pop() + " ";
+    }
+    return prefix;
 }
